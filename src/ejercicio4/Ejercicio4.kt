54291d04423e9bb99f57f0cb4b7e133580f442ee
DiegoCastroT <diegodct6@gmail.com>
2024-11-26 19:23:28 +0100

    Added initial check revision. Need to refactor and improve it

--- src/ejercicio4/Ejercicio4.kt
+++ src/ejercicio4/Ejercicio4.kt
@@ -5,9 +5,29 @@ fun main() {
     val atleta = arrayListOf("Correr", "Saltar", "Correr", "Saltar", "Correr")
     val pista = "_|_|_"
 
-    checkRace(atleta, pista)
+    println("$pista ${checkRace(atleta, pista)}")
 }
 
-fun checkRace(atleta: ArrayList<String>, pista: String) {
+fun checkRace(atleta: ArrayList<String>, pista: String): Boolean {
 
+    if (atleta.size != pista.length) return false
+
+    for (i in atleta.indices) {
+        if (atleta[i].equals("Correr")) {
+            if (pista[i] == '_') {
+                continue
+            } else {
+                return false
+            }
+        } else if (atleta[i].equals("Saltar")) {
+            if (pista[i] == '|') {
+                continue
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+    return true
 }
