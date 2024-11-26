bd7476d7d5b82d6dcc1d0efbc289db9d6d5dd6e4
DiegoCastroT <diegodct6@gmail.com>
2024-11-26 19:10:39 +0100

    Initial exercise 4 structure

---
+++ src/ejercicio4/Ejercicio4.kt
@@ -0,0 +1,13 @@
+package ejercicio4
+
+fun main() {
+
+    val atleta = arrayListOf("Correr", "Saltar", "Correr", "Saltar", "Correr")
+    val pista = "_|_|_"
+
+    checkRace(atleta, pista)
+}
+
+fun checkRace(atleta: ArrayList<String>, pista: String) {
+
+}
