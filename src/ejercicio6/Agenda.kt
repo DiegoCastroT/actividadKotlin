60792fb6dfa7727ee4c35eeac3aac3f5a7f40c94
DiegoCastroT <diegodct6@gmail.com>
2024-12-02 18:20:13 +0100

    Added agenda functions need to implement and check it

--- src/ejercicio6/Agenda.kt
+++ src/ejercicio6/Agenda.kt
@@ -1,7 +1,65 @@
 package ejercicio6
 
-class Agenda (var arrayList: ArrayList<Contacto>, var maxSize:Int = 10){
+//Nombre unique
+class Agenda(private var contacts: ArrayList<Contacto>, private var maxSize: Int = 3) {
 
-    
+    fun addContact(name: String, tel: String): Boolean {
 
+        if (contacts.size >= maxSize) {
+            return false
+        } else {
+            for (contact in contacts) {
+                if (contact.nombre == name) {
+                    return false
+                }
+            }
+            val contacto = Contacto(name, tel)
+            contacts.add(contacto)
+            return true
+        }
+    }
+
+    fun listContacts() {
+        for (contact in contacts) {
+            println("Nombre: ${contact.nombre} Teléfono: ${contact.telefono}")
+        }
+    }
+
+    fun getContact(name: String): String {
+        for (contact in contacts) {
+            if (contact.nombre == name) {
+                return contact.telefono
+            }
+        }
+        return "No encontrado"
+    }
+
+    fun isContact(name: String): Boolean {
+
+        for (contact in contacts) {
+            if (contact.nombre == name) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun deleteContact(name:String):Boolean{
+        for(contacto in contacts){
+            if(contacto.nombre == name){
+                contacts.remove(contacto)
+                return true
+            }
+        }
+        return false
+    }
+
+    fun getCurrentSize():Int{
+        return contacts.size - maxSize
+    }
+
+    fun isFull():Boolean{
+        return contacts.size == maxSize
+    }
 }
+
--- src/ejercicio6/Ejercicio6.kt
+++ src/ejercicio6/Ejercicio6.kt
@@ -5,9 +5,67 @@ fun main() {
     println("Bienvenido, esta es tu agenda de contactos.")
     println("Escoge una opción")
     var success = false
+    val agenda = Agenda(ArrayList<Contacto>(), 3)
+
+    fun showMenu() {
+        println("1. Añadir un contacto a la agenda")
+        println("2. Listar todos los contactos de la agenda.")
+        println("3. Buscar un contacto por su nombre.")
+        println("4. Comprobar si un contacto existe.")
+        println("5. Eliminar un contacto")
+        println("6. Indicar el número de contactos que se pueden añadir en la agenda, es decir, indicar el número de huecos libres.")
+        println("7. Comprobar si la agenda está llena.")
+        println("8. Salir")
+    }
+
+    fun addContact() {
+        println("Dime el nombre del contacto")
+        val name = readln()
+        println("Dame el teléfono")
+        val tel = readln()
+        if(agenda.addContact(name,tel)){
+            println("Añadido correctamente")
+        }else{
+            println("Error al añadir")
+        }
+    }
+
+    fun listAllContacts() {
+        agenda.listContacts()
+    }
+
+    fun searchContact() {
+        println("Dime el nombre del contacto")
+        val name = readln()
+        println(agenda.getContact(name))
+    }
+
+    fun checkIfExists() {
+        println("Dime el nombre del contacto")
+        val name = readln()
+        if(agenda.isContact(name)){
+            println("Existe el contacto")
+        }else{
+            println("No existe el contacto")
+        }
+    }
+
+    fun deleteContact() {
+        println("Dime el nombre del contacto")
+        val name = readln()
+        agenda.deleteContact(name)
+    }
+
+
+    fun getStorage() {
+    }
+
+    fun checkFull() {
+    }
 
     while (!success) {
-        var input = readln().toInt()
+        showMenu()
+        val input = readln().toInt()
 
         when (input) {
             1 -> addContact()
@@ -17,57 +75,11 @@ fun main() {
             5 -> deleteContact()
             6 -> getStorage()
             7 -> checkFull()
-            8 -> exit()
-
-
+            8 -> success = true
+            else -> {
+                println("Incorrecto")
+                continue
+            }
         }
     }
-
-
-}
-
-fun showMenu() {
-    println("1. Añadir un contacto a la agenda")
-    println("2. Listar todos los contactos de la agenda.")
-    println("3. Buscar un contacto por su nombre.")
-    println("4. Comprobar si un contacto existe.")
-    println("5. Eliminar un contacto")
-    println("6. Indicar el número de contactos que se pueden añadir en la agenda, es decir, indicar el número de huecos libres.")
-    println("7. Comprobar si la agenda está llena.")
-    println("8. Salir")
-
-
-}
-
-
-fun addContact() {
-
-}
-
-fun listAllContacts() {
-
-}
-
-fun searchContact() {
-
-}
-
-fun deleteContact() {
-
-}
-
-fun checkIfExists() {
-
-}
-
-fun getStorage() {
-
-}
-
-fun checkFull() {
-
-}
-
-fun exit() {
-
 }
